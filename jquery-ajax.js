944c32b494c9168bb038d253b18a69d6213f8e31
HazMupp <peter_heier@hotmail.com>
2016-05-04 10:45:27 +0200

    add get request and textStatus
    text shows upp in tag with a given id

--- jquery-ajax.js
+++ jquery-ajax.js
@@ -2,7 +2,17 @@
 
 // Använd jQuery och ajax
 
-$('#my-button').click(function() {  console.log("knapped klickades")  } );
-
-
+$('#my-button').click(function() {
+  console.log("knapped klickades")
+  
+  // Gör en GET request med AJAX i jQuery
+  $.get("https://api.github.com/zen", function( data, textStatus, jqXHR) {
+    // Vilken status
+    console.log(testStatus);
+    
+    // Visa texten från servern i stycket <p> med id_t shw-server-tet
+    $('show-server-text').text(data); // # inte nådvändigt, men funkar att ha med om man vill
+  })
+   }
+);
 
